97b7d6eafc22689efe0dc2f9d377d69de221ffaf
Ogun Akar <ogunakar09@gmail.com>
2023-10-17 16:08:49 +0300

    feat: add billing section sub section ui

--- src/components/billing/index.tsx
+++ src/components/billing/index.tsx
@@ -1,7 +1,24 @@
 import "./styles.scss";
 
 const Billing = () => {
-  return <div className="billing">billing</div>;
+  return (
+    <div className="billing">
+      <span className="billing__title">Billing</span>
+      <div className="billing__sections">
+        <div className="billing__order">order</div>
+        <div className="billing__sub">
+          <span className="billing__sub__title">Your plan</span>
+          <div className="billing__sub__type">
+            <span className="billing__sub__type__subtitle">Pro Annual</span>
+            <span className="billing__sub__type__date">
+              Renews on Nov. 2021
+            </span>
+          </div>
+          <button className="billing__sub__button">Cancel subscription</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Billing;
